8dd0bb933e6578da5eee51bb2e1f2cc846edef47
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2025-04-29 09:54:46 -0500

    Add greedy search policy to

--- rl_sensors/envs/test_graph_search.py
+++ rl_sensors/envs/test_graph_search.py
@@ -2,12 +2,23 @@ from rl_sensors.envs.graph_search import GraphSearchEnv
 import matplotlib.pyplot as plt
 import numpy as np
 
+def greedy_search(obs):
+  angles = obs['node_features'][..., 0]
+  weights = obs['node_features'][..., 1]
+  
+  # Select the angle with the highest weight
+  max_index = np.argmax(weights)
+  action = np.array([angles[max_index]])
+  return action
+
 if __name__ == '__main__':
   env = GraphSearchEnv()
   
   obs, _ = env.reset(seed=0)
   for i in range(500):
-    obs, reward, _, _, _ = env.step(env.action_space.sample())
+    # action = greedy_search(obs)
+    action = env.action_space.sample()
+    obs, reward, _, _, _ = env.step(action)
     env.render()
     print(reward)
     plt.pause(0.01)
